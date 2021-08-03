d65bd01ce65bc2d98bbfbe7639a71709ee8be2cb
bddwyx <bddwyx@sjtu.edu.cn>
2021-08-03 18:37:57 +0800

    TCP socket

--- src/monitor/scripts/MonitorSocket.py
+++ src/monitor/scripts/MonitorSocket.py
@@ -1,1 +1,23 @@
 import socket
+
+class MonitorSocket:
+    # 创建套接字 socket
+    TCPServerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def __init__(self, port):
+        self.__port = port
+        # 绑定本地信息 bind
+        self.TCPServerSocket.bind(("", port))
+        # 让默认的套接字由主动变为被动 listen
+        self.TCPServerSocket.listen(128)
+        # 等待客户端的链接 accept
+        self.ClientSocket, clientAddr = self.TCPServerSocket.accept()
+        print(clientAddr)
+
+    def __del__(self):
+        self.TCPServerSocket.close()
+        self.ClientSocket.close()
+        print("Connection deinit on port %d", self.__port)
+
+    def ReceiveAPackage(self):
+        return self.ClientSocket.recv(1024)
--- src/monitor/scripts/main.py
+++ src/monitor/scripts/main.py
@@ -4,8 +4,10 @@ import MonitorSocket
 import HL7Process
 import MonitorPublisher
 
+MonitorSocket1 = MonitorSocket(7890)
+
 if __name__ == "__main__":
     try:
         DataRecord()
-    except:
-        pass
+    except Exception as e:
+        print(repr(e))
