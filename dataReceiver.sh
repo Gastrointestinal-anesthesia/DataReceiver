7939a4ffac7c35003d03f8a9f785c9d95a10a1a1
sd-cloud <mnbvcxz12l@163.com>
2021-08-07 12:14:26 +0800

    Merge into main

--- dataReceiver.sh
+++ dataReceiver.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 source devel/setup.bash
-<<<<<<< HEAD
+
 cd src/monitor/scripts
 chmod 777 main.py
-=======
->>>>>>> main
+
 {
 gnome-terminal -t "runMonitor" -x bash -c "rosrun monitor main.py"
 }&
